1d9c63f79325396600c6393edcae4510aa672fe5
Ayush Mukherjee <111524872+Ayush5421@users.noreply.github.com>
2023-09-13 19:31:25 +0530

    Add files via upload

---
+++ Task 3 Calculator/scripts.js
@@ -0,0 +1,17 @@
+let outputScreen = document.getElementById("output-screen");
+function display(num) {
+  outputScreen.value += num;
+}
+function Calculate() {
+  try {
+    outputScreen.value = eval(outputScreen.value);
+  } catch (err) {
+    alert("This is invalid");
+  }
+}
+function Clear() {
+  outputScreen.value = " ";
+}
+function del() {
+  outputScreen.value = outputScreen.value.slice(0, -1);
+}
